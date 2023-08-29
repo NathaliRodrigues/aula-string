ee72e64aae14f003c4d7e426af1171afbbff4d9b
NathaliRodrigues <142436453+NathaliRodrigues@users.noreply.github.com>
2023-08-29 17:03:33 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+//Nathali Rodrigues 
+
+//exercicio 1
+/*
+a. indefinido
+b.null 
+c.11
+d.0
+e.indefinido
+indefinido
+indefinido
+
+
+// exercicio 2
+SUBI NUM ÔNIBUS EM MIROCOS
+27
+
+//exercicio 2 b
+let nome = prompt("Qual seu nome?");
+let email = prompt("Qual seu e-mail?");
+console.log(`O email ${email} foi cadastrado com sucesso. Seja Bem-Vindo(a) ${nome}`);
+
+
+let comidas =["lasanha", "pizza", "sagu", "churrasco", "torta de bolacha"];
+console.log(comidas);
+console.log('Essas são as minhas comidas preferidas');
+console.log(comidas[0]);
+console.log(comidas[1]);
+console.log(comidas[2]);
+console.log(comidas[3]);
+console.log(comidas[4]);
+
+let comidadousuario = prompt("Qual sua comida preferida?");
+console.log(comidas);
+comidas.splice(1,1);
+comidas[1] = comidadousuario;
+console.log(comidas);
+*/
+
+//exercicio 2 b 3
+/*
+let listaDeTarefas = []
+let tarefa0 = prompt ("Cite uma tarefa que voce realiza no seu dia a dia?");
+let tarefa1 = prompt ("Cite uma tarefa que voce realiza no seu dia a dia?");
+let tarefa2 = prompt ("Cite uma tarefa que voce realiza no seu dia a dia?");
+listaDeTarefas.push(tarefa0 ,tarefa1, tarefa2);
+console.log (listaDeTarefas);
+
+listaDeTarefas.splice(Number(prompt('Escreva um numero de 0 a 2?')));
+console.log(listaDeTarefas);
+*/
